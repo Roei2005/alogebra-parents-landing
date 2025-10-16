a9de82dbdf77edcbabd0f329413e4c4418cc8b69
Roei2005 <roeky77@gmail.com>
2025-10-16 23:31:31 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+// src/main.tsx
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async' // ⭐️ ייבוא חדש ⭐️
 import './index.css'
-import App from './App'
+import App from './App' // זה אמור להיות Servicez אם הוא הרכיב הראשי, אבל נשתמש בשם App כרגע
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>    {/* ⭐️ עטיפה חדשה ⭐️ */}
+      <App />
+    </HelmetProvider>   {/* ⭐️ סגירת עטיפה ⭐️ */}
   </React.StrictMode>
 )
