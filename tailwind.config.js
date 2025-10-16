e8da13687c29dc70887711ffad451587aea1d93e
Roei2005 <roeky77@gmail.com>
2025-10-16 15:20:54 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Assistant", "system-ui", "Arial"],
+        heading: ["Rubik", "Assistant", "system-ui", "Arial"],
+      },
+    },
+  },
   plugins: [],
 }
